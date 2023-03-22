d2555cbc8d11ce04f8a97008ed11d7955241abc6
Esidede <audreymapfearon@gmail.com>
2023-03-22 06:40:48 -0700

    tst

---
+++ add_node.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+* addnode - add node to top of stack
+* @head: first node
+* @n: new value
+*
+* Return: none
+*/
+void addnode(stack_t **head, int n)
+{
+	stack_t *new_node, *temp;
+
+	temp = *head;
+	new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	if (temp)
+		temp->prev = new_node;
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
