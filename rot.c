3b396c6ad7dcf222a47e594f701233edb21ff2d0
Esidede <audreymapfearon@gmail.com>
2023-03-22 06:32:27 -0700

    task

---
+++ rot.c
@@ -0,0 +1,56 @@
+#include "monty.h"
+
+/**
+* f_rotl- function that rotates the stack to the top
+* @head: head of the stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+{
+	stack_t *tmp = *head, *aux;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	aux = (*head)->next;
+	aux->prev = NULL;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = tmp;
+	(*head) = aux;
+}
+#include "monty.h"
+
+/**
+* f_rotr - function that rotates the stack to the bottom
+* @head: stack head of the stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *copy;
+
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
+}
