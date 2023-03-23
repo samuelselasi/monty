3063949bf18ab69d1f98cf13ddafa203cff1b37f
samuelselasi <samelsekasi@gmail.com>
2023-03-23 11:33:27 -0700

    Add function to print string

--- print_string.c
+++ print_string.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 
 /**
-* f_pstr - function that prints the string starting at the top of the stack,
-* followed by a new
-* @head: head of stack
-* @counter: line count
-*
-* Return: nothing
-*/
+ * f_pstr - print string at the top of stack
+ * @head: first node of linked list
+ * @counter: line counter
+ *
+ * Return: none
+ */
+
 void f_pstr(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
